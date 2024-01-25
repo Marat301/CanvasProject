19171b1f08c29e9eb8e8b67c965db067927129a1
Marat301 <maratbickbau@gmail.com>
2024-01-25 01:14:49 -0500

    Closes Issues 1-5

---
+++ App.LearningManagement/Library.LearningManagement/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.LearningManagement.Models {
+    public class Person {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Dictionary<int, double> Grades { get; set; }
+        public char Classification { get; set; }
+
+        public Person() {
+            Name = string.Empty;
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
+
